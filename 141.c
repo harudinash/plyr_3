15893d856f65543334ae9c14eec79cb341c44a65
harudinash <46379892+harudinash@users.noreply.github.com>
2019-03-24 19:58:30 +0530

    Create 141.c

---
+++ 141.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l,m,n;
+    char a[1000][1000],b[1000],c[1000];
+    
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+     scanf("%s",a[i]);
+
+//for(i=0;i<n;i++)
+  //   printf("%s ",a[i]);
+     
+     for(i=0;i<n;i++)
+     {
+         k=0;
+         strcpy(b,a[i]);
+         strcpy(c,a[i+1]);
+         for(j=0;b[j]!='\0';j++)
+         {
+             if(b[j]!=c[j])
+          {
+              k=1;
+              break;
+          }
+         }
+         if(k==0)
+         {
+         printf("yes");
+         break;
+         }
+     }
+     
+    return 0;
+}
