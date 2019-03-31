5422f4bc0d702cdd63e4558b6f33863271a212f3
harudinash <46379892+harudinash@users.noreply.github.com>
2019-03-31 18:15:28 +0530

    Create 160.c

---
+++ 160.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l,m,n,r,o=1,a[10000];
+    printf("Input :\n");
+    scanf("%d%d",&n,&m);
+    printf("Output :\n");
+    
+    l=m|n;
+    while(l)
+    {
+        r=l%2;
+     
+        l=l/2;
+     if(r==1)
+        {
+            i++;
+        }
+    }
+    
+     printf("%d",i);
+    return 0;
+}
