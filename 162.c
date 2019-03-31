10826d36b475747aef92523a587cc294065e4a06
harudinash <46379892+harudinash@users.noreply.github.com>
2019-03-31 18:44:51 +0530

    Create 162.c

---
+++ 162.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l,m,n,r;
+    char a[10000];
+    printf("Input :\n");
+    scanf("%d%d",&n,&m);
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",a);
+        for(j=0;a[j]!='\0';j++)
+        {
+            if(a[j]=='a'||a[j]=='e'||a[j]=='i'||a[j]=='o'||a[j]=='u')
+           {
+               l++;
+            //printf("l %d",l);
+            break;
+           }
+        }
+        
+        
+    }
+    printf("Output :\n");
+   
+    if(l>=m)
+    printf("yes");
+    else
+    printf("no");
+    
+    
+    
+    
+    return 0;
+}
