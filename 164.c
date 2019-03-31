f6f4bf86c1fe53b23ebfc2d90e726956ea65e288
harudinash <46379892+harudinash@users.noreply.github.com>
2019-03-31 19:19:22 +0530

    Create 164.c

---
+++ 164.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l,m,n,r,a[10000];
+    //char a[10000];
+    printf("Input :\n");
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    printf("Output :\n");
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            
+        
+        if(a[i]>a[j])
+        {
+            l=a[i];
+            a[i]=a[j];
+            a[j]=l;
+        }
+        
+        }
+        
+    }
+    for(i=n-1;i>=0;i--)
+    
+    {
+        if(a[i]==m)
+        {
+            printf("%d",a[i]);
+            break;
+        }
+        else if(a[i]<m)
+        {
+            printf("%d",a[i]);
+        break;
+        
+        }
+    }
+    
+    
+    
+    return 0;
+}
